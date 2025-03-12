1ae1575af5f7cccafe9090dd64b29ec59477e3b9
nykyforuk_vasyl <nikiforuk.vasya@gmail.com>
2025-03-12 02:02:17 +0200

    link

--- src/app/(admin)/companies/page.tsx
+++ src/app/(admin)/companies/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Header from '@/app/components/header';
-import Toolbar from '../../components/toolbar';
-import SearchInput from '../../components/search-input';
-import AddCompanyButton from '../../components/add-company-button';
-import CompanyTable from '../../components/company-table';
-import CompanyRow from '../../components/company-row';
-import { Status } from '../../components/status-label';
+import Toolbar from '@/app/components/toolbar';
+import SearchInput from '@/app/components/search-input';
+import AddCompanyButton from '@/app/components/add-company-button';
+import CompanyTable from '@/app/components/company-table';
+import CompanyRow from '@/app/components/company-row';
+import { Status } from '@/app/components/status-label';
 
 export interface PageProps {}
 
@@ -21,7 +21,7 @@ export default function Page({}: PageProps) {
           <CompanyRow
             id={1}
             category="Products"
-            company="Costro"
+            company="Costco"
             status={Status.Pending}
             promotion={true}
             country="USA"
--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Sidebar from '../components/sidebar';
+import Sidebar from '@/app/components/sidebar';
 
 export interface LayoutProps {
   children: React.ReactNode;
